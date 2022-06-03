a5fedbea6c32dcf6570cf76389913639ba23c3a8
Galuh Angga Saputra <42699186+lutegglh@users.noreply.github.com>
2022-06-04 00:21:41 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -12,7 +12,7 @@ import shutil
 
 from time import sleep, time
 from telegram.ext import CommandHandler
-from telegram.message import message
+from telegram.message import Message
 from telegram import InlineKeyboardMarkup
 from fnmatch import fnmatch
 
@@ -49,6 +49,7 @@ ariaDlManager.start_listener()
 class MirrorListener(listeners.MirrorListeners):
     def __init__(self, bot, update, pswd, isTar=False, extract=False, isZip=False, isQbit=False, isLeech=False):
         super().__init__(bot, update)
+        self.Message = message
         self.isTar = isTar
         self.extract = extract
         self.isZip = isZip

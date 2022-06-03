a8b54eca999395655883fdb20bf5c089439170fc
Galuh Angga Saputra <42699186+lutegglh@users.noreply.github.com>
2022-06-03 08:29:09 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -312,7 +312,7 @@ class GoogleDriveHelper:
                 self.updater.cancel()
                 if self.is_cancelled:
                     return
-        self.__listener.onUploadComplete(link, size, self.__total_folders, mime_type, self.name)
+        self.__listener.onUploadComplete(link, size, folders, mime_type, self.name)
         return link
 
 

bb6fd55f9ae6b9268d23c40b4151e2798f51f50a
Galuh Angga Saputra <42699186+lutegglh@users.noreply.github.com>
2022-06-28 17:45:49 +0700

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -112,9 +112,9 @@ def sendStatusMessage(msg, bot):
     free = get_readable_file_size(free)
     currentTime = get_readable_time(time.time() - botStartTime)
     progress, buttons = get_readable_message()
-    progress += f"<b>🖥️CPU:</b> <code>{psutil.cpu_percent()}%</code>" \
-           f" <b>💾RAM:</b> <code>{psutil.virtual_memory().percent}%</code>" \
-           f" <b>💽DISK:</b> <code>{psutil.disk_usage('/').percent}%</code>"
+    progress += f"<b>🖥️ CPU:</b> <code>{psutil.cpu_percent()}%</code>" \
+           f" <b>💾 RAM:</b> <code>{psutil.virtual_memory().percent}%</code>" \
+           f" <b>\n💽 DISK:</b> <code>{psutil.disk_usage('/').percent}%</code>"
     with download_dict_lock:
         dlspeed_bytes = 0
         uldl_bytes = 0
@@ -132,7 +132,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>💿FREE:</b> <code>{free}</code> | <b>☕UPTIME:</b> <code>{currentTime}</code>\n<b>DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>UL:</b> <code>{ulspeed}/s</code> 🔺\n"
+        progress += f"\n<b>💿 FREE:</b> <code>{free}</code>\n<b>☕ UPTIME:</b> <code>{currentTime}</code>\n<b>🔻DL:</b> <code>{dlspeed}/s</code> | <b>🔺UL:</b> <code>{ulspeed}/s</code>\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:

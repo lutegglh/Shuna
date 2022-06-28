1ddd26eadb7ab6f9bac432c667ec6d406a574fc4
Galuh Angga Saputra <42699186+lutegglh@users.noreply.github.com>
2022-06-28 16:54:48 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -9,7 +9,6 @@ from bot import dispatcher, download_dict, download_dict_lock, STATUS_LIMIT
 from telegram import InlineKeyboardMarkup
 from telegram.ext import CallbackQueryHandler
 from bot.helper.telegram_helper import button_build, message_utils
-from bot.modules.mirror import onUploadComplete
 
 LOGGER = logging.getLogger(__name__)
 
@@ -143,7 +142,7 @@ def get_readable_message():
                     msg += f"\n<b>🔺 Uploaded:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> of <code>{download.size()}</code>"
                 else:
                     msg += f"\n<b>🔻 Downloaded:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> of <code>{download.size()}</code>"
-                msg += f"\n<b>🚀 Speed:</b> <code>{download.speed()}</code>\n<b>⏳ Sisa Waktu:</b> <code>{download.eta()}</code>\n👤 {uname} | #️⃣ {self.uid}"
+                msg += f"\n<b>🚀 Speed:</b> <code>{download.speed()}</code>\n<b>⏳ Sisa Waktu:</b> <code>{download.eta()}</code>"
                 try:
                     msg += f"\n<b>🌱 Seeders:</b> <code>{download.aria_download().num_seeders}</code>" \
                            f" | <b>🌱 Peers:</b> <code>{download.aria_download().connections}</code>"

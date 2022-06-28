2a7c72dad09312691c7ea3c1f23273a3817cbb54
Galuh Angga Saputra <42699186+lutegglh@users.noreply.github.com>
2022-06-28 16:18:41 +0700

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -20,8 +20,8 @@ def _watch(bot: Bot, update, isTar=False, isZip=False, isLeech=False):
         msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] untuk mirror dengan youtube-dl.\n\n"
         msg += "<b>Catatan: Jika ingin memilih Resolusi video, mengubah nama video, atau download audio only, ikuti cara ini</b>\n\nKualitas yang bisa di mirror: audio, 144, 240, 360, 480, 720, 1080, 2160."
         msg += "\n\nJika ingin memberi nama sesuka hati, ketik setelah memberi simbol ini |"
-        msg += f"\n\nContoh:\n<code>/{BotCommands.WatchCommand} https://youtu.be/blablablabla 720 |Shuna</code>\n\n"
-        msg += "Maka file tersebut akan dimirror di Resolusi 720p, dan akan miliki nama <b>Shuna</b>"
+        msg += f"\n\nContoh:\n<code>/{BotCommands.WatchCommand} https://youtu.be/blablablabla 720 |Shion</code>\n\n"
+        msg += "Maka file tersebut akan dimirror di Resolusi 720p, dan akan miliki nama <b>Shion</b>"
         sendMessage(msg, bot, update)
         return
     

405084f65828e939d6f787512a687b038aeab6be
Galuh Angga Saputra <42699186+lutegglh@users.noreply.github.com>
2022-07-04 23:48:58 +0700

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -90,7 +90,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>💿FREE:</b> <code>{free}</code> | <b>☕UPTIME:</b> <code>{currentTime}</code>\n<b>DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>UL:</b> <code>{ulspeed}/s</code> 🔺\n"
+        msg += f"\n<b>💿 FREE:</b> <code>{free}</code> | <b>☕ UPTIME:</b> <code>{currentTime}</code>\n<b>🔻 DL:</b> <code>{dlspeed}/s</code> | <b>🔺 UL:</b> <code>{ulspeed}/s</code>\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
